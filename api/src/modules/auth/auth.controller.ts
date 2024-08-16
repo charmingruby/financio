6e215ccd6461dd1e061147aa86ed403d11d8e9c8
charmingruby <mrrubycanal3105@gmail.com>
2024-08-16 09:02:36 -0300

    feat: authenticate service, but with insecure jwt secret yet

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 
 import { AuthService } from './auth.service'
+import { AuthenticateDto } from './dto/authenticate.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  authenticate(@Body() dto: AuthenticateDto) {
+    return this.authService.authenticate(dto)
+  }
 }
--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+
+import { UsersRepository } from '@/shared/database/repositories/users.repositories'
+
+import { AuthenticateDto } from './dto/authenticate.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersRepo: UsersRepository,
+  ) {}
+
+  async authenticate(dto: AuthenticateDto) {
+    const { email, password } = dto
+
+    const user = await this.usersRepo.findUnique({
+      where: { email },
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials.')
+    }
+
+    const isPasswordValid = await compare(password, user.password)
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials.')
+    }
+
+    const accessToken = await this.jwtService.signAsync({ sub: user.id })
+
+    return { accessToken }
+  }
+}
