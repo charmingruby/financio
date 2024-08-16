742db882988707716ea1905843107580fedc3a26
charmingruby <mrrubycanal3105@gmail.com>
2024-08-16 09:22:50 -0300

    feat: adds environment config

---
+++ api/src/shared/config/env.ts
@@ -0,0 +1,25 @@
+import { plainToInstance } from 'class-transformer'
+import { IsNotEmpty, IsString, validateSync } from 'class-validator'
+
+class Env {
+  @IsString()
+  @IsNotEmpty()
+  jwtSecret: string
+
+  @IsString()
+  @IsNotEmpty()
+  databaseUrl: string
+}
+
+export const env: Env = plainToInstance(Env, {
+  databaseUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+})
+
+console.log(`jwt ${process.env.JWT_SECRET}`)
+
+const errors = validateSync(env)
+
+if (errors.length > 0) {
+  throw new Error(JSON.stringify(errors, null, 2))
+}
